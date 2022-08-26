0137e7f5889ceb8aa1b066083699f7b081a155b4
Abhishek Bisht <abhishekbisht567@gmail.com>
2022-08-26 11:32:19 -0400

    second commit on modifying simple.java file

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class simple{
              public static void main(string args[])}
-             system out println("hello");
+             system out println("hello add line");
               }
 
 }
